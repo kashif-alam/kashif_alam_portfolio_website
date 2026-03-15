b54eaeb9b9600d3ed77a129031025f3da086de8d
Kashif Alam <kashifalam.ai@gmail.com>
2026-03-16 03:33:16 +0500

    favicon added

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    publicDir: 'public',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
@@ -53,6 +54,7 @@
     build: {
       target: 'esnext',
       outDir: 'build',
+      copyPublicDir: true,
     },
     server: {
       port: 3000,
